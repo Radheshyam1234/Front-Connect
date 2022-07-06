2209541bad0363c7a524e073ff13ffa01b8260ff
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-07-06 21:30:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loadPosts } from "./features/Posts/postSlice";
