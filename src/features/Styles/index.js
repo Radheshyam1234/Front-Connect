f2849aa68c291ef89c96eaf01e22ba7849467bfe
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-07-07 20:02:25 +0530

    Update index.js

--- src/features/Styles/index.js
+++ src/features/Styles/index.js
@@ -2,5 +2,4 @@ export * from "./postCardStyle";
 export * from "./filterStyles";
 export * from "./avatarStyle";
 export * from "./formStyles";
-export * from "./navStyles";
 export * from "./navigationStyle";
