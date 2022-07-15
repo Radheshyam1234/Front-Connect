7f605f1c9912a507c9576128dd46cfb6994cf7d9
Radheshyam Kr <88738458+Radheshyam1234@users.noreply.github.com>
2022-07-15 13:17:29 +0530

    feat[added like and bookmark the post]

---
+++ src/features/Posts/LikesContainer.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getUsersWhoLikedThePost,
+  resetUsersWhoLikedThePost,
+} from "./postSlice";
+import { usePostSelector } from "./postSlice";
+import {
+  useDisclosure,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+import { UserHorizontalCard } from "../Users/UserHorizontalCard";
+import { likesCounterStyle } from "../Styles";
+
+export const LikesContainer = ({ post }) => {
+  const [loading, setLoading] = useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
+  const { usersWhoLikedPost } = usePostSelector();
+  const totalLikes = post.likes.length;
+
+  const handleOpenLikeContainer = () => {
+    onOpen();
+    setLoading(true);
+    const promise = dispatch(getUsersWhoLikedThePost({ postId: post._id }));
+    promise.finally(() => setLoading(false));
+  };
+
+  const handleCloseLikeContainer = () => {
+    onClose();
+    dispatch(resetUsersWhoLikedThePost());
+  };
+  return (
+    <>
+      {totalLikes ? (
+        <Text
+          {...likesCounterStyle}
+          onClick={handleOpenLikeContainer}
+          cursor="pointer"
+        >
+          {totalLikes} likes
+        </Text>
+      ) : (
+        ""
+      )}
+
+      <Modal isOpen={isOpen} onClose={handleCloseLikeContainer}>
+        <ModalOverlay />
+        <ModalContent maxH="25rem" overflow="auto">
+          <ModalHeader>Likes</ModalHeader>
+
+          {loading ? (
+            "Loading"
+          ) : (
+            <>
+              {usersWhoLikedPost?.map((user) => (
+                <UserHorizontalCard key={user._id} user={user} />
+              ))}
+            </>
+          )}
+          <ModalCloseButton />
+          <ModalBody></ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
--- src/features/Posts/PostCard.js
+++ src/features/Posts/PostCard.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Linkify from "react-linkify";
-
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { FaRegCommentDots } from "react-icons/fa";
 import { BsBookmark, BsFillBookmarkFill } from "react-icons/bs";
 import { MdOutlineMoreVert } from "react-icons/md";
-import { getTime, copyLink } from "./utils";
-import { ComponentDecorator } from "../../Utils/ComponentDecorator";
 
 import {
   Avatar,
@@ -15,6 +12,7 @@ import {
   Button,
   Text,
   Icon,
+  IconButton,
   Image,
   Flex,
   Spacer,
@@ -26,8 +24,19 @@ import {
   AvatarBadge,
   AlertDialogContent,
   AlertDialogOverlay,
+  useMediaQuery,
+  Center,
 } from "@chakra-ui/react";
 import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+
+import { useDispatch } from "react-redux";
+import { useAuthentication } from "../Authentication/AuthenticationSlice";
+import { getTime, copyLink } from "./utils";
+import { ComponentDecorator } from "../../Utils/ComponentDecorator";
+import { followTheUser, unFollowTheUser } from "../Followers/FollowersSlice";
+import { LikesContainer } from "./LikesContainer";
+import { deletePost, likeThePost, removeLikeFromPost } from "./postSlice";
+import { bookMarkPost, removeBookmarkPost } from "./postSlice";
 
 import {
   postCardWrapperStyle,
@@ -38,17 +47,44 @@ import {
   postImageStyle,
   cardActionsStyle,
   cardActionIcon,
-  commentsCounterStyle,
-  likesCounterStyle,
 } from "../Styles";
 
 export const PostCard = ({ post }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const cancelRef = React.useRef();
+  const dispatch = useDispatch();
+  const { user } = useAuthentication();
+  const [likeBtnDisabled, setLikeBtnDisabled] = useState(false);
+  const [bookmarkDisabled, setBookmarkDisabled] = useState(false);
+
+  const likeUnlikeHandler = ({ type }) => {
+    setLikeBtnDisabled(true);
+    if (type === "like") {
+      dispatch(likeThePost({ postId: post?._id })).finally(() => {
+        setLikeBtnDisabled(false);
+      });
+    } else {
+      dispatch(removeLikeFromPost({ postId: post?._id })).finally(() => {
+        setLikeBtnDisabled(false);
+      });
+    }
+  };
+  const bookmarkRemoveBookmarkHandler = ({ type }) => {
+    setBookmarkDisabled(true);
+    if (type === "bookmark")
+      dispatch(bookMarkPost(post._id)).finally(() => {
+        setBookmarkDisabled(false);
+      });
+    else {
+      dispatch(removeBookmarkPost(post._id)).finally(() => {
+        setBookmarkDisabled(false);
+      });
+    }
+  };
 
   return (
     <>
-      <Box {...postCardWrapperStyle}>
+      <Box {...postCardWrapperStyle} maxW={{ base: "20rem", sm: "35rem" }}>
         <Box {...postCardUserInfoStyle}>
           <Avatar src={post?.postedBy?.profilephoto} {...smallAvatarStyle} />
 
@@ -66,41 +102,64 @@ export const PostCard = ({ post }) => {
                 <Icon as={MdOutlineMoreVert} />
               </MenuButton>
               <MenuList>
-                <>
-                  <MenuItem onClick={onOpen}>Delete</MenuItem>
-                  <AlertDialog
-                    isOpen={isOpen}
-                    leastDestructiveRef={cancelRef}
-                    onClose={onClose}
-                    isCentered
-                  >
-                    <AlertDialogOverlay>
-                      <AlertDialogContent>
-                        <AlertDialogBody>
-                          Are you sure want to delete this post ?
-                        </AlertDialogBody>
-                        <AlertDialogFooter>
-                          <Button ref={cancelRef} onClick={onClose}>
-                            Cancel
-                          </Button>
-                          <Button
-                            colorScheme="red"
-                            onClick={() => {
-                              onClose();
-                            }}
-                            ml={3}
-                          >
-                            Delete
-                          </Button>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialogOverlay>
-                  </AlertDialog>
-                </>
+                {post?.postedBy?._id === user?._id && (
+                  <>
+                    <MenuItem onClick={onOpen}>Delete</MenuItem>
+                    <AlertDialog
+                      isOpen={isOpen}
+                      leastDestructiveRef={cancelRef}
+                      onClose={onClose}
+                      isCentered
+                    >
+                      <AlertDialogOverlay>
+                        <AlertDialogContent>
+                          <AlertDialogBody>
+                            Are you sure want to delete this post ?
+                          </AlertDialogBody>
+                          <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                              Cancel
+                            </Button>
+                            <Button
+                              colorScheme="red"
+                              onClick={() => {
+                                dispatch(deletePost(post?._id));
+                                onClose();
+                              }}
+                              ml={3}
+                            >
+                              Delete
+                            </Button>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialogOverlay>
+                    </AlertDialog>
+                  </>
+                )}
 
                 <MenuItem onClick={() => copyLink({ post })}>Share</MenuItem>
 
-                <MenuItem>Follow {post?.postedBy?.firstName}</MenuItem>
+                {post?.postedBy?._id !== user?._id && (
+                  <>
+                    {user?.following?.includes(post?.postedBy?._id) ? (
+                      <MenuItem
+                        onClick={() => {
+                          dispatch(unFollowTheUser(post?.postedBy?.userName));
+                        }}
+                      >
+                        Unfollow {post?.postedBy?.firstName}
+                      </MenuItem>
+                    ) : (
+                      <MenuItem
+                        onClick={() => {
+                          dispatch(followTheUser(post?.postedBy?.userName));
+                        }}
+                      >
+                        Follow {post?.postedBy?.firstName}
+                      </MenuItem>
+                    )}
+                  </>
+                )}
               </MenuList>
             </Menu>
           </Box>
@@ -115,18 +174,52 @@ export const PostCard = ({ post }) => {
 
         <Flex {...cardActionsStyle}>
           <Box>
-            <Icon as={AiOutlineHeart} {...cardActionIcon} />
+            {post?.likes?.includes(user?._id) ? (
+              <IconButton
+                isDisabled={likeBtnDisabled}
+                color="red"
+                icon={<AiFillHeart />}
+                {...cardActionIcon}
+                onClick={() => {
+                  likeUnlikeHandler({ type: "removelike" });
+                }}
+              />
+            ) : (
+              <IconButton
+                isDisabled={likeBtnDisabled}
+                icon={<AiOutlineHeart />}
+                {...cardActionIcon}
+                onClick={() => {
+                  likeUnlikeHandler({ type: "like" });
+                }}
+              />
+            )}
           </Box>
-          <Box ml="4">
-            <Icon as={FaRegCommentDots} {...cardActionIcon} />
-          </Box>
+
           <Spacer />
           <Box>
-            <Icon as={BsBookmark} {...cardActionIcon} />
+            {user?.savedpost?.includes(post?._id) ? (
+              <IconButton
+                isDisabled={bookmarkDisabled}
+                icon={<BsFillBookmarkFill />}
+                {...cardActionIcon}
+                onClick={() => {
+                  bookmarkRemoveBookmarkHandler({ type: "removeBookmark" });
+                }}
+              />
+            ) : (
+              <IconButton
+                isDisabled={bookmarkDisabled}
+                icon={<BsBookmark />}
+                {...cardActionIcon}
+                onClick={() => {
+                  bookmarkRemoveBookmarkHandler({ type: "bookmark" });
+                }}
+              />
+            )}
           </Box>
         </Flex>
-        <Text {...likesCounterStyle}>8 likes</Text>
-        <Text {...commentsCounterStyle}>View all 5 comments</Text>
+        <LikesContainer post={post} />
       </Box>
     </>
   );
